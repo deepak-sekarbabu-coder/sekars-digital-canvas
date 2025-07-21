e89e5abef7314b8c8eddf8fe75ce4d11a58821e8
Deepak Sekarbabu <princedeepak2012@gmail.com>
2025-07-21 22:34:54 +0200

    Check Github Page

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: process.env.NODE_ENV === 'production' ? '/sekars-digital-canvas/' : '/',
   server: {
     host: '::',
     port: 8080,
